ab9b9e25d2c94a6133b896498e55390f502a26c8
Krzysztof Druciak <krzysztof.druciak@ericsson.com>
2017-05-15 08:00:49 +0200

    dodano obsluge termometru

--- arduino/CAR_PC/CAR_PC.ino
+++ arduino/CAR_PC/CAR_PC.ino
@@ -1,16 +1,39 @@
 #include "ELM327.h"
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+#define ONE_WIRE_BUS 5
+OneWire oneWire(ONE_WIRE_BUS);
+
+// Pass our oneWire reference to Dallas Temperature.
+DallasTemperature sensors(&oneWire);
+#define TEMPERATURE_PRECISION 12
+// Ilość czujników
+#define SENSORS_NUM 2
+// Adresy czujników
+// arrays to hold device addresses
+DeviceAddress insideThermometer = {
+  0x28, 0x80, 0x3E, 0x64, 0x4, 0x0, 0x0, 0xD2
+};
 
 ELM327 elm327;
 const byte stateHC05pin = 3;
 const byte ATpin = 4;
 String bufor;
-
+double temperatureInside() {
+  sensors.requestTemperatures(); // Send the command to get temperatures
+  return sensors.getTempC(insideThermometer);
+}
 void setup()
 {
   Serial.begin(9600);
+
+  sensors.begin();
+  sensors.setResolution(insideThermometer , TEMPERATURE_PRECISION);
+
   elm327.init(38400, stateHC05pin, ATpin);
   Serial.println("wait for connection");
-  
+
   while (!elm327.isConnectedToBluetooth()) {
     Serial.print('.');
   }
@@ -29,15 +52,17 @@ void loop()
       USEFUL::freeMode();
       bufor = "";
     }
-    bufor="";
+    bufor = "";
   }
   //Serial.println("working");
   Serial.print(elm327.getVoltage());
   Serial.println(" VOLT");
   Serial.print(elm327.engineCoolantTemperature());
   Serial.println(" temperature");
-   Serial.print(elm327.fuelTankLevel());
+  Serial.print(elm327.fuelTankLevel());
   Serial.println(" paliwo");
+  Serial.print(temperatureInside());
+  Serial.println(" temperatura");
   delay(100);
 }
 
